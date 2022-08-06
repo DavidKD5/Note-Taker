37a1eefe6e25d120d709f297e7e1b219ceae3a0e
DavidKD5 <jdbetancourt6@gmail.com>
2022-08-05 19:16:37 -0600

    establish routes complete

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const notes = require("./db/db.json");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/db", "db.json"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  var newNote = req.body;
+
+  fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      var allNotes = JSON.parse(data);
+
+      allNotes.push(newNote);
+
+      fs.writeFile(
+        "./Develop/db/db.json",
+        JSON.stringify(allNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info("Successfully updated notes!")
+      );
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
