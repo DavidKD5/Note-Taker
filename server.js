6af4f6fa678cf1ddcc52f0f81281f62fd1648f93
DavidKD5 <jdbetancourt6@gmail.com>
2022-08-05 19:52:57 -0600

    changing file structure

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use(express.static("public"));
 const PORT = process.env.PORT || 3001;
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/db", "db.json"));
+  res.sendFile(path.join(__dirname, "./db", "db.json"));
 });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.post("/api/notes", (req, res) => {
