39ea7519c3a2aa65d00de72b4bf12e5ef8cb8bc9
DavidKD5 <jdbetancourt6@gmail.com>
2022-08-05 19:35:09 -0600

    linking html css and js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
-const notes = require("./db/db.json");
-
+const fs = require("fs");
 const app = express();
 
 app.use(express.json());
@@ -11,13 +10,13 @@ app.use(express.static("public"));
 const PORT = process.env.PORT || 3001;
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Develop/db", "db.json"));
+  res.sendFile(path.join(__dirname, "./Develop/db", "db.json"));
 });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.post("/api/notes", (req, res) => {
